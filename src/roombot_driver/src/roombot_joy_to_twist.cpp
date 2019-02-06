61ad4e241e0117b0d97f05c1d6e5dbc6544ca882
rigelVY <taisei218@gmail.com>
2019-02-07 00:47:21 +0900

    create "develop" branch.

--- src/roombot_driver/src/roombot_joy_to_twist.cpp
+++ src/roombot_driver/src/roombot_joy_to_twist.cpp
@@ -11,7 +11,7 @@ void joy_callback(const sensor_msgs::Joy& joy_msg)
 {
     cmd_vel.linear.x =joy_msg.axes[1];
     cmd_vel.linear.y =joy_msg.axes[0];
-    cmd_vel.angular.z=joy_msg.axes[2];
+    cmd_vel.angular.z=joy_msg.axes[3];
 }
 
 int main(int argc, char **argv){
