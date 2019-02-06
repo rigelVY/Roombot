eb14d7176ebe6dc5f80ce65303dae1106034b1af
rigelVY <taisei218@gmail.com>
2019-02-06 18:27:38 +0900

    first build.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rigel/ROS_ws/Roombot/src
+# Build directory: /home/rigel/ROS_ws/Roombot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(roombot_driver)
