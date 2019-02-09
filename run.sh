ef941a709baaf9dcfa077fd8756f48600e684752
rigelVY <taisei218@gmail.com>
2019-02-09 19:20:06 +0900

    Edit ./run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#ラズパイとPCをROSで連携させるための設定
+#export ROS_IP=`hostname -I`
+#export ROS_MASTER_URI=http://192.168.11.14:11311/
+
+#USBの権限がない場合に使用
+#sudo chmod a+rw /dev/ttyUSB0
+
+
+# realsense D435iのセンサデータ出力用launchファイルとroombot_driverのlaunchファイルを実行
+gnome-terminal -e "bash -c 'source /opt/ros/kinetic/setup.bash; source ~/ROS_ws/catkin_ws/devel/setup.bash; roslaunch realsense2_camera rs_camera.launch'"
+sleep 3s
+gnome-terminal -e "bash -c 'source /opt/ros/kinetic/setup.bash; source ~/ROS_ws/Roombot/devel/setup.bash; roslaunch roombot_driver move_robot.launch joy:=true'"
